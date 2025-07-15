aad7d55c43239af52d68784b928a8927c3804410
Gabbyjerad66 <gabrieljerad35@gmail.com>
2025-07-15 10:48:30 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,6 +1,6 @@
  const session = process.env.SESSION || '';
-const mycode = process.env.CODE || "254";
-const botname = process.env.BOTNAME || 'DREADED';
+const mycode = process.env.CODE || "254112842772";
+const botname = process.env.BOTNAME || 'Gabby';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
 const database = process.env.DATABASE_URL || '';
